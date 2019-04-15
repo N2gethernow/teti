1073a17bfdd22d7c6239dd7d262b2b4584e1fe8b
Pawel Lesiecki <pawel.lesiecki@allegro.pl>
2019-04-15 16:09:00 +0200

    --regular3G, TTI

--- puppeteer-runner.js
+++ puppeteer-runner.js
@@ -1,6 +1,24 @@
+const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 const ua = 'Mozilla/5.0 (Linux; Android 8.0.0; Nexus 5X Build/OPR4.170623.006) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3756.0 Mobile Safari/537.36';
+
+const NETWORK_PRESETS = {
+    Good3G: {
+        offline: false,
+        downloadThroughput: 1.5 * 1024 * 1024 / 8,
+        uploadThroughput: 750 * 1024 / 8,
+        latency: 40
+    },
+    Regular3G: {
+        offline: false,
+        downloadThroughput: 750 * 1024 / 8,
+        uploadThroughput: 250 * 1024 / 8,
+        latency: 100
+    }
+};
+
+const NETWORK_PRESET = process.argv.includes('--regular3G') ? NETWORK_PRESETS.Regular3G : NETWORK_PRESETS.Good3G;
 
 module.exports = async function (url) {
     const browser = await puppeteer.launch({
@@ -16,12 +34,7 @@ module.exports = async function (url) {
 
     const page = await browser.newPage();
     const client = await page.target().createCDPSession();
-    await client.send('Network.emulateNetworkConditions', {
-        offline: false,
-        downloadThroughput: 1.5 * 1024 * 1024 / 8,
-        uploadThroughput: 750 * 1024 / 8,
-        latency: 40
-    });
+    await client.send('Network.emulateNetworkConditions', NETWORK_PRESET);
     await client.send('Emulation.setCPUThrottlingRate', { rate: 4 });
     await page.setUserAgent(ua);
     await page.setDefaultTimeout(60 * 1000);
@@ -32,19 +45,19 @@ module.exports = async function (url) {
     // });
     // await page.evaluateOnNewDocument(longTaskObserverCode);
 
-//     await page.evaluateOnNewDocument(`
-//     !function(){if('PerformanceLongTaskTiming' in window){var g=window.__tti={e:[]};
-// g.o=new PerformanceObserver(function(l){g.e=g.e.concat(l.getEntries())});
-// g.o.observe({entryTypes:['longtask']})}}();`);
+    await page.evaluateOnNewDocument(`
+    !function(){if('PerformanceLongTaskTiming' in window){var g=window.__tti={e:[]};
+g.o=new PerformanceObserver(function(l){g.e=g.e.concat(l.getEntries())});
+g.o.observe({entryTypes:['longtask']})}}();
+${fs.readFileSync(require.resolve('tti-polyfill'))}`);
     await page.goto(url);
-    // await page.addScriptTag({ path: require.resolve('tti-polyfill') });
 
     const { timing, paint, mark } = await page.evaluate(async () => ({
         timing: JSON.stringify(window.performance.timing),
         paint: JSON.stringify(performance.getEntriesByType('paint')),
         mark: JSON.stringify(
             performance.getEntriesByType('mark')
-                // .concat({ name: 'time-to-interactive', startTime: await window.ttiPolyfill.getFirstConsistentlyInteractive() })
+                .concat({ name: 'time-to-interactive', startTime: await window.ttiPolyfill.getFirstConsistentlyInteractive() })
         )
     }));
 
@@ -57,5 +70,5 @@ module.exports = async function (url) {
             paint: JSON.parse(paint),
             mark: JSON.parse(mark)
         });
-    })
-}
+    });
+};
